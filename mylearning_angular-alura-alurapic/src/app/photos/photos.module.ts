3ca815b360347444a32fd2c874e97eeccdd97db2
Rafael Viana <rafaelvinicius@gmail.com>
2021-12-24 10:45:43 -0300

    Alurapic, copia do instagram para  aprendizagem usando Angular. - Alurapic, copy of instagram for learning using Angular.

---
+++ mylearning_angular-alura-alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { CardModule } from '../shared/components/card.module';
+
+@NgModule({    
+    imports: [ 
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        HttpClientModule,
+        CommonModule 
+    ]
+})
+export class PhotosModule {}
